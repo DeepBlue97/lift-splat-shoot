2eace60fe5b1fcbd7d8a731a7ae26addf0b8304a
DeepBlue97 <945048933@qq.com>
2022-12-12 17:46:41 +0800

    m

---
+++ run.sh
@@ -0,0 +1,11 @@
+NUSCENES_ROOT=/home/YQ_Wang/ocean/datasets/nuscenes
+python main.py train trainval --dataroot=$NUSCENES_ROOT --logdir=./runs --gpuid=0
+tensorboard --logdir=./runs --bind_all
+
+python main.py train mini --dataroot=$NUSCENES_ROOT --logdir=./runs --gpuid=0
+
+python main.py train mini/trainval --dataroot=$NUSCENES_ROOT --logdir=./runs --gpuid=0
+tensorboard --logdir=./runs --bind_all
+
+python main.py train mini/trainval --dataroot=NUSCENES_ROOT --logdir=./runs --gpuid=0
+tensorboard --logdir=./runs --bind_all
